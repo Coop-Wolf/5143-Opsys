84d99328f4759207a2856d2cfe482db88feeed53
Coop-Wolf <156962773+Coop-Wolf@users.noreply.github.com>
2025-10-21 16:34:19 +0000

    ran test run

--- Assignments/P02/arcade_test.py
+++ Assignments/P02/arcade_test.py
@@ -1,22 +1,20 @@
 import arcade
 
-# Constants for the window
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 400
-SCREEN_TITLE = "My First Arcade Window"
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "My Game"
 
-# Create a window class
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.AMAZON)
 
-    # This is called every time the window needs to draw
     def on_draw(self):
-        arcade.start_render()
-        arcade.draw_text("Hello Arcade!", 200, 200, arcade.color.WHITE, 24)
+        # Only clear the screen at the start of each frame
+        self.clear()  
+        # Draw everything else here
+        arcade.draw_text("Hello Arcade!", 100, 300, arcade.color.WHITE, 24)
 
-# Run the game
 def main():
     window = MyGame()
     arcade.run()
