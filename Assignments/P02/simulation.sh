109149463f540e6882b0e524f2a629f9604ad01e
Coop-Wolf <156962773+Coop-Wolf@users.noreply.github.com>
2025-11-20 22:07:23 +0000

    updated csv files.

--- Assignments/P02/simulation.sh
+++ Assignments/P02/simulation.sh
@@ -41,24 +41,44 @@
 # python3 main.py file_num=26 path=io sched=srtf cpus=2 ios=2
 # python3 main.py file_num=26 path=io sched=priority cpus=2 ios=2
 
-# NEEDS TO BE 27 AND PATH=dl for all these
-python3 main.py file_num=26 path=io sched=fcfs cpus=1 ios=1
-python3 main.py file_num=26 path=io sched=rr cpus=1 ios=1
-python3 main.py file_num=26 path=io sched=sjf cpus=1 ios=1
-python3 main.py file_num=26 path=io sched=srtf cpus=1 ios=1
-python3 main.py file_num=26 path=io sched=priority cpus=1 ios=1
-python3 main.py file_num=26 path=io sched=fcfs cpus=1 ios=2
-python3 main.py file_num=26 path=io sched=rr cpus=1 ios=2
-python3 main.py file_num=26 path=io sched=sjf cpus=1 ios=2
-python3 main.py file_num=26 path=io sched=srtf cpus=1 ios=2
-python3 main.py file_num=26 path=io sched=priority cpus=1 ios=2
-python3 main.py file_num=26 path=io sched=fcfs cpus=2 ios=1
-python3 main.py file_num=26 path=io sched=rr cpus=2 ios=1
-python3 main.py file_num=26 path=io sched=sjf cpus=2 ios=1
-python3 main.py file_num=26 path=io sched=srtf cpus=2 ios=1
-python3 main.py file_num=26 path=io sched=priority cpus=2 ios=1
-python3 main.py file_num=26 path=io sched=fcfs cpus=2 ios=2
-python3 main.py file_num=26 path=io sched=rr cpus=2 ios=2
-python3 main.py file_num=26 path=io sched=sjf cpus=2 ios=2
-python3 main.py file_num=26 path=io sched=srtf cpus=2 ios=2
-python3 main.py file_num=26 path=io sched=priority cpus=2 ios=2
+# python3 main.py file_num=27 path=dl sched=fcfs cpus=1 ios=1
+# python3 main.py file_num=27 path=dl sched=rr cpus=1 ios=1
+# python3 main.py file_num=27 path=dl sched=sjf cpus=1 ios=1
+# python3 main.py file_num=27 path=dl sched=srtf cpus=1 ios=1
+# python3 main.py file_num=27 path=dl sched=priority cpus=1 ios=1
+# python3 main.py file_num=27 path=dl sched=fcfs cpus=1 ios=2
+# python3 main.py file_num=27 path=dl sched=rr cpus=1 ios=2
+# python3 main.py file_num=27 path=dl sched=sjf cpus=1 ios=2
+# python3 main.py file_num=27 path=dl sched=srtf cpus=1 ios=2
+# python3 main.py file_num=27 path=dl sched=priority cpus=1 ios=2
+# python3 main.py file_num=27 path=dl sched=fcfs cpus=2 ios=1
+# python3 main.py file_num=27 path=dl sched=rr cpus=2 ios=1
+# python3 main.py file_num=27 path=dl sched=sjf cpus=2 ios=1
+# python3 main.py file_num=27 path=dl sched=srtf cpus=2 ios=1
+# python3 main.py file_num=27 path=dl sched=priority cpus=2 ios=1
+# python3 main.py file_num=27 path=dl sched=fcfs cpus=2 ios=2
+# python3 main.py file_num=27 path=dl sched=rr cpus=2 ios=2
+# python3 main.py file_num=27 path=dl sched=sjf cpus=2 ios=2
+# python3 main.py file_num=27 path=dl sched=srtf cpus=2 ios=2
+# python3 main.py file_num=27 path=dl sched=priority cpus=2 ios=2
+
+python3 main.py file_num=28 path=net sched=fcfs cpus=1 ios=1
+python3 main.py file_num=28 path=net sched=rr cpus=1 ios=1
+python3 main.py file_num=28 path=net sched=sjf cpus=1 ios=1
+python3 main.py file_num=28 path=net sched=srtf cpus=1 ios=1
+python3 main.py file_num=28 path=net sched=priority cpus=1 ios=1
+python3 main.py file_num=28 path=net sched=fcfs cpus=1 ios=2
+python3 main.py file_num=28 path=net sched=rr cpus=1 ios=2
+python3 main.py file_num=28 path=net sched=sjf cpus=1 ios=2
+python3 main.py file_num=28 path=net sched=srtf cpus=1 ios=2
+python3 main.py file_num=28 path=net sched=priority cpus=1 ios=2
+python3 main.py file_num=28 path=net sched=fcfs cpus=2 ios=1
+python3 main.py file_num=28 path=net sched=rr cpus=2 ios=1
+python3 main.py file_num=28 path=net sched=sjf cpus=2 ios=1
+python3 main.py file_num=28 path=net sched=srtf cpus=2 ios=1
+python3 main.py file_num=28 path=net sched=priority cpus=2 ios=1
+python3 main.py file_num=28 path=net sched=fcfs cpus=2 ios=2
+python3 main.py file_num=28 path=net sched=rr cpus=2 ios=2
+python3 main.py file_num=28 path=net sched=sjf cpus=2 ios=2
+python3 main.py file_num=28 path=net sched=srtf cpus=2 ios=2
+python3 main.py file_num=28 path=net sched=priority cpus=2 ios=2
