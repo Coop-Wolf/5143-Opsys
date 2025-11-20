7b224f9e56d88f58a0b0e7799e2fd669d1e19d72
Coop-Wolf <156962773+Coop-Wolf@users.noreply.github.com>
2025-11-20 20:07:48 +0000

    another simulation on file 26

--- Assignments/P02/simulation.sh
+++ Assignments/P02/simulation.sh
@@ -20,6 +20,28 @@
 # python3 main.py file_num=25 path=cpu sched=srtf cpus=2 ios=2
 # python3 main.py file_num=25 path=cpu sched=priority cpus=2 ios=2
 
+# python3 main.py file_num=26 path=io sched=fcfs cpus=1 ios=1
+# python3 main.py file_num=26 path=io sched=rr cpus=1 ios=1
+# python3 main.py file_num=26 path=io sched=sjf cpus=1 ios=1
+# python3 main.py file_num=26 path=io sched=srtf cpus=1 ios=1
+# python3 main.py file_num=26 path=io sched=priority cpus=1 ios=1
+# python3 main.py file_num=26 path=io sched=fcfs cpus=1 ios=2
+# python3 main.py file_num=26 path=io sched=rr cpus=1 ios=2
+# python3 main.py file_num=26 path=io sched=sjf cpus=1 ios=2
+# python3 main.py file_num=26 path=io sched=srtf cpus=1 ios=2
+# python3 main.py file_num=26 path=io sched=priority cpus=1 ios=2
+# python3 main.py file_num=26 path=io sched=fcfs cpus=2 ios=1
+# python3 main.py file_num=26 path=io sched=rr cpus=2 ios=1
+# python3 main.py file_num=26 path=io sched=sjf cpus=2 ios=1
+# python3 main.py file_num=26 path=io sched=srtf cpus=2 ios=1
+# python3 main.py file_num=26 path=io sched=priority cpus=2 ios=1
+# python3 main.py file_num=26 path=io sched=fcfs cpus=2 ios=2
+# python3 main.py file_num=26 path=io sched=rr cpus=2 ios=2
+# python3 main.py file_num=26 path=io sched=sjf cpus=2 ios=2
+# python3 main.py file_num=26 path=io sched=srtf cpus=2 ios=2
+# python3 main.py file_num=26 path=io sched=priority cpus=2 ios=2
+
+# NEEDS TO BE 27 AND PATH=dl for all these
 python3 main.py file_num=26 path=io sched=fcfs cpus=1 ios=1
 python3 main.py file_num=26 path=io sched=rr cpus=1 ios=1
 python3 main.py file_num=26 path=io sched=sjf cpus=1 ios=1
